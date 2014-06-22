06aa5bc5feae8c0d8bf1d02589bd899f7e85e5e3
Kris Reeck <kris.reeck@gmail.com>
2014-06-22 15:11:58 -0400

    abstract titlebar for key type viewing

---
+++ src/views/types/_titlebar.blade.php
@@ -0,0 +1,10 @@
+<div id="page-title">
+    <div id="key-type">{{ $Object['type'] }}</div>
+    <div id="key-name"><i class="fa fa-key fa-fw"></i> {{ $Object['name'] }}</div>
+    <div id="key-options" data-database="{{ \Session::get('activeDatabase') }}" data-key="{{ $Object['name'] }}" data-ttl="{{ $Object['ttl'] }}">
+        <a class="" href="" title="Edit Key"> <i class="fa fa-pencil"></i> </a>
+        <a href="#" title="Rename Key"><i class="fa fa-filter"></i></a>
+        <a href="#" title="Set TTL for Key"><i class="fa fa-clock-o"></i></a>
+        <a class="ajaxDelete" href="{{ REDBOOK_URI }}key.delete" title="Remove Key"><i class="fa fa-trash-o"></i></a>
+    </div>
+</div>
