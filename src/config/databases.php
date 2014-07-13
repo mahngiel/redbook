168df07a991d0f4c6e99ba79f947655f0fc8472d
Kris Reeck <kris.reeck@gmail.com>
2014-07-12 21:03:28 -0400

    add namespace separator to configs

--- src/config/databases.php
+++ src/config/databases.php
@@ -5,18 +5,21 @@ return array(
         'host'     => '127.0.0.1',
         'port'     => 1666,
         'database' => 0,
-        'password' => 'devicevault'
+        'password' => 'devicevault',
+        'namespace' => ':',
     ),
     'metrics' => array(
         'host'     => '127.0.0.1',
         'port'     => 1666,
         'database' => 1,
-        'password' => 'devicevault'
+        'password' => 'devicevault',
+        'namespace' => ':',
     ),
     'redbook' => array(
         'host'     => '127.0.0.1',
         'port'     => 2245,
         'database' => 0,
-        'password' => 'redbook'
+        'password' => 'redbook',
+        'namespace' => ':',
     ),
 );
